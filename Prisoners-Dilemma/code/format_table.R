52738343943539087583f643c28717d40ddbf3b9
Liam Andrew Beattie <159044283+22562435@users.noreply.github.com>
2024-10-17 22:53:58 +0200

    working

--- Prisoners-Dilemma/code/format_table.R
+++ Prisoners-Dilemma/code/format_table.R
@@ -1,6 +1,6 @@
 library(kableExtra)
 
-format_table <- function(data, title = "Strategy Rankings Across Different p Values") {
+format_table2 <- function(data, title = "Strategy Rankings Across Different p Values") {
     data %>%
         kable("latex", align = 'c', caption = title, booktabs = TRUE, linesep = "") %>%
         kable_styling(
@@ -10,6 +10,6 @@ format_table <- function(data, title = "Strategy Rankings Across Different p Val
         ) %>%
         column_spec(1, bold = TRUE, width = "4cm") %>%  # Bold the strategy names
         column_spec(2:(ncol(data)), width = "0.7cm") %>%  # Adjust the width of payoff columns
-        # add_header_above(c(" " = 1, "Rankings Across p Values" = ncol(data) - 1)) %>%
+        add_header_above(c(" " = 1, "p Values" = ncol(data) - 1)) %>%
         row_spec(seq(5, nrow(data)-5, by = 5), extra_latex_after = "\\midrule")  # Add a horizontal line every 5 rows for better readability
 }
